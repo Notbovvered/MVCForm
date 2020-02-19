49f31541e2fc8fad560317e09fad9cac897981c2
Joseph <Jkhassan95@gmail.com>
2020-02-19 11:59:57 +0000

    Completed Simple form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCForm.Models;
 
 namespace MVCForm.Controllers
 {
@@ -11,6 +12,18 @@ namespace MVCForm.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+       
+        [HttpGet]
+        public IActionResult SimpleBinding()
+        {
+            return View(new WebUser() { FirstName = "Joseph", LastName = "Hassan" });
+        }
+        [HttpPost]
+        public IActionResult SimpleBinding(WebUser webUser)
+        {
+            //TODO: Update in DB here...
+            return Content($"User {webUser.FirstName} updated!");
         }
     }
 }
--- Models/WebUser.cs
+++ Models/WebUser.cs
@@ -7,5 +7,7 @@ namespace MVCForm.Models
 {
     public class WebUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
